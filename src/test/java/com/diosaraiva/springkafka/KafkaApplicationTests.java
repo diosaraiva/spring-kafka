32db1f0fb06d0fd37c01a3f60d5d07e79814e6b0
Diogo Saraiva <diosaraiva@hotmail.com>
2022-04-09 20:52:08 -0300

    Example from Baeldung ok!

---
+++ src/test/java/com/diosaraiva/springkafka/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diosaraiva.springkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandioCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
